3b27c6787de7d03839a78f2520b3d4d97444a3bd
Ilan Landau <ilanlandau98@gmail.com>
2025-09-07 23:30:12 +0300

    Add total revenue and monthly revenue queries (formatted & chronologically ordered)

--- dvd_rental_project/queries.sql
+++ dvd_rental_project/queries.sql
@@ -2,3 +2,15 @@
 -- Author: Ilan Landau
 -- Database: PostgreSQL (dvdrental)
 -- This file will store the queries developed during practice.
+
+-- 1. Total Revenue:
+Select SUM (amount) from payment;
+
+--2. Revenue per month:
+SELECT
+  TO_CHAR(DATE_TRUNC('month', payment_date), 'Mon/YYYY') AS month,
+  ROUND(SUM(amount), 2) AS revenue
+FROM payment
+GROUP BY DATE_TRUNC('month', payment_date)
+ORDER BY DATE_TRUNC('month', payment_date);
+
